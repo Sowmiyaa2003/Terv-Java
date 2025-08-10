97b5f374351b069fd5efa5fe5e881c4872f471a7
Sowmiyaa2003 <sekaranchandru550@gmail.com>
2025-08-10 15:53:02 +0530

    Update and rename test.java to jdbcconnection.java

--- jdbcconnection.java
+++ jdbcconnection.java
@@ -2,7 +2,7 @@
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.Scanner;
-public class test {
+public class jdbcconnection {
 
     public  static void main(String[] args){
         String url="jdbc:mysql://localhost:3306/";
@@ -44,3 +44,4 @@ public class test {
     }
     
 }
+
