bfec2b17d3b73c5ad4b6b4cc6f00601263c3d9e2
Sowmiyaa2003 <sekaranchandru550@gmail.com>
2025-08-11 10:43:16 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,8 @@
+//reverse a string
+            /*public class demo{
+                public static void main(String[] args) {
+                    StringBuffer sb=new StringBuffer("Java is a fungame");
+                    sb.reverse();
+                    System.out.print(sb);
+                    }
+                }
