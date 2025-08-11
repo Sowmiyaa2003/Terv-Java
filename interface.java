e3637e3aa2e95b34a192915aa69fa885f2bdf723
Sowmiyaa2003 <sekaranchandru550@gmail.com>
2025-08-11 10:44:38 +0530

    Create interface.java

---
+++ interface.java
@@ -0,0 +1,47 @@
+/*  class parent1{
+                            int x=10;
+                            void parent1method(){
+                        }
+                            class parent2{
+                                int y=20;
+                                void parent2method(){
+                            }
+                            class child extends parent1{
+                                void parent1method(){
+                                System.out.println(x);
+                            }}
+                            class child extends parent2{
+                                void parent2method(){
+                                System.out.println(y);
+                            }}
+                            public class demo{
+                                public static void main(String[] args) {
+                                    child c=new child();
+                                    c.parent1method();
+                                    c.parent2method();
+                                }
+                            }
+                                */
+               /* interface parent1{
+                 int x=10;
+                void parent1method();
+            }
+            interface parent2{
+                int y=20;
+                void parent2method();
+            }
+             class child implements parent1,parent2{
+                public void parent1method(){
+                System.out.println(x);
+            }
+            public void parent2method(){
+            System.out.println(y);
+            }
+        }
+        public class demo{
+            public static void main(String[] args) {
+                child c=new child();
+                c.parent1method();
+                c.parent2method();
+            }
+        }
