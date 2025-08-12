b5f7be39d6aced9995ebbfa157cbcbf71c86b995
Sowmiyaa2003 <sekaranchandru550@gmail.com>
2025-08-12 14:40:37 +0530

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,51 @@
+//for loop
+/*
+for(let a=0;a<=10;a++)
+console.log(a)*/
+//while loop
+/*let a=1
+while(a<=5)
+    a++
+console.log(a) //6
+*/
+//for...arrays
+/*for(let num of[10,20,30]){
+    console.log(num)
+}
+    */
+   //for....in(objects)
+   /*
+let object = {
+  name: "Sowmiyaa",
+  age: 21,
+  course: "JavaScript"
+};
+for(let key in object){
+    console.log(object[key])
+}
+    */
+   //sum of array
+   /*let a=[10,20,30,40,50]
+   let sum=0
+   for(let i=0;i<a.length;i++)
+    sum+=a[i]
+console.log(sum)*/
+//while using sum of array
+/*let a=[1,2,3,4,5]
+let sum=0
+let i=0
+while(i<a.length){
+sum+=a[i]
+i++
+}
+console.log(sum)*/
+//for...arrays
+/*let sum=0;
+let a=[1,2,3]
+for(let num of a){
+    sum+=num
+}
+    console.log(sum)
+    */
+
+    
