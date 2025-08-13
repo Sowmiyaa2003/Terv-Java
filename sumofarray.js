ce59abff341ddb9a0946fd7fa211bd6dcc12b383
Sowmiyaa2003 <sekaranchandru550@gmail.com>
2025-08-13 10:05:40 +0530

    Create sumofarray.js

---
+++ sumofarray.js
@@ -0,0 +1,22 @@
+ //sum of array
+   /*let a=[10,20,30,40,50]
+   let sum=0
+   for(let i=0;i<a.length;i++)
+    sum+=a[i]
+console.log(sum)*/
+/*let a=[1,2,3,4,5]
+let sum=0
+let i=0
+while(i<a.length){
+sum+=a[i]
+i++
+}
+console.log(sum)*/
+/*let sum=0;
+let a=[1,2,3]
+for(let num of a){
+    sum+=num
+}
+    console.log(sum)
+    */
+   
